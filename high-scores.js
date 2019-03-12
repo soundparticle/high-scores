b5ca81eb830a10ba17cca1333cf0f7a3eac862eb
Antreo Pukay <acrosswalker@yahoo.com>
2019-03-11 20:52:20 -0700

    scores equals input passing

---
+++ high-scores.js
@@ -0,0 +1,11 @@
+class HighScores {
+  constructor(input) {
+    this.input = input;
+    this.scores = input;
+    console.log('****** this.input *****', this.input);
+  }
+}
+
+module.exports = {
+  HighScores,
+};
